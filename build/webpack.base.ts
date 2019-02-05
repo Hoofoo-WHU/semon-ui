d8d8c940e7e5c3fe3e55b82426601afdc1f4b03c
Hoofoo <wangshuophone@icloud.com>
2019-02-05 15:04:59 +0800

    配置react

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -3,10 +3,31 @@ import * as webpack from 'webpack'
 import * as HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const config: webpack.Configuration = {
-  entry: path.resolve('src/index.js'),
+  entry: path.resolve('src/index.jsx'),
   output: {
     path: path.resolve('dist'),
     filename: '[name]-[hash:8].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
