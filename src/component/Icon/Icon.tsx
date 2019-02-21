2d64d8daea76ea1da9308c4416aa72a5d3c2e438
Hoofoo <wangshuophone@icloud.com>
2019-02-21 14:55:48 +0800

    icon 添加自定义类

--- src/component/Icon/Icon.tsx
+++ src/component/Icon/Icon.tsx
@@ -3,6 +3,7 @@ import SVG, { types as iconTypes } from './SVG'
 import styled from '@/style/component/Icon.scss'
 
 interface IProps {
+  className?: string
   style?: React.CSSProperties
   type: iconTypes
 }
@@ -10,8 +11,11 @@ interface IProps {
 class Icon extends React.Component<IProps> {
   render() {
     const SvgIcon = SVG[this.props.type]
+    const classes = []
+    this.props.className && classes.push(this.props.className)
+    classes.push(styled.icon)
     return (
-      <SvgIcon className={styled.icon} style={this.props.style} viewBox='64 64 896 896'></SvgIcon>
+      <SvgIcon className={classes.join(' ')} style={this.props.style} viewBox='64 64 896 896'></SvgIcon>
     )
   }
 }
