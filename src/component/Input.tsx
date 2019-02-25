938ea9b81ac7ce2d103d12d21f1593dfdd2e10dd
Hoofoo <wangshuophone@icloud.com>
2019-02-25 11:56:53 +0800

    📝重构Input的Props导出，添加Input测试

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,39 +1,33 @@
 import * as React from 'react'
 import styled from '@/style/component/Input.scss'
-interface IProps {
-  size?: 'small' | 'large'
-  disabled?: boolean
-  defaultValue?: string
-  value?: string
-  placeholder?: string
-  prefix?: React.ReactNode
-  suffix?: React.ReactNode
-  className?: string
-  readOnly?: boolean
-  onFocus?: React.FocusEventHandler<HTMLInputElement>
-  onBlur?: React.FocusEventHandler<HTMLInputElement>
-  onChange?: React.ChangeEventHandler<HTMLInputElement>
-  onPressEnter?: React.KeyboardEventHandler<HTMLInputElement>
-  onKeyPress?: React.KeyboardEventHandler<HTMLInputElement>
+import classMerge from '../until/class-merge'
+import { tuple } from '../until/type'
+import * as PropTypes from 'prop-types'
+
+const InputSize = tuple('small', 'large')
+type InputSize = typeof InputSize[number]
+
+const propTypes = {
+  size: PropTypes.oneOf(InputSize),
+  disabled: PropTypes.bool,
+  defaultValue: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  prefix: PropTypes.node,
+  suffix: PropTypes.node,
+  className: PropTypes.string,
+  readOnly: PropTypes.bool,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onPressEnter: PropTypes.func,
+  onKeyPress: PropTypes.func,
 }
 
-class Input extends React.Component<IProps> {
+class Input extends React.Component<Input.Props> {
   static displayName = 'Input'
-  private classes() {
-    const { size, className } = this.props
-    const classes = []
-    className && classes.push(className)
-    classes.push(styled.input)
-    size && classes.push(styled[size])
-    return classes.join(' ')
-  }
-  private wrapperClasses() {
-    const { size, className } = this.props
-    const classes = [styled.input, styled['input-wrapper-affix-wrapper']]
-    className && classes.unshift(className)
-    size && classes.push(styled[size])
-    return classes.join(' ')
-  }
+  static propTypes = propTypes
+
   private focusHandle(e: React.FocusEvent<HTMLInputElement>) {
     this.props.onFocus && this.props.onFocus(e)
   }
@@ -45,13 +39,14 @@ class Input extends React.Component<IProps> {
   }
   private keyPressHandle(e: React.KeyboardEvent<HTMLInputElement>) {
     this.props.onKeyPress && this.props.onKeyPress(e)
-    switch (e.key) {
-      case 'Enter':
+    switch (e.key.toLowerCase()) {
+      case 'enter':
         this.props.onPressEnter && this.props.onPressEnter(e)
     }
   }
-  render() {
-    const { placeholder, prefix, suffix, defaultValue, value, disabled, readOnly } = this.props
+
+  private renderAffixInput() {
+    const { placeholder, prefix, suffix, defaultValue, value, disabled, readOnly, className, size } = this.props
     const props = {
       placeholder,
       defaultValue,
@@ -63,17 +58,70 @@ class Input extends React.Component<IProps> {
       onChange: this.changeHandle.bind(this),
       onKeyPress: this.keyPressHandle.bind(this)
     }
-    if (prefix || suffix) {
-      return <span className={this.wrapperClasses()} data-disabled={disabled}>
-        {prefix ? <span className={styled.prefix}>{prefix}</span> : null}
-        <input
-          className={styled['innert-input']}
-          {...props}
-        />
-        {suffix ? <span className={styled.suffix}>{suffix}</span> : null}
-      </span>
+    const classes = classMerge(
+      className,
+      styled.input,
+      styled['input-wrapper-affix-wrapper'],
+      styled[size]
+    )
+    return <span className={classes} data-disabled={disabled}>
+      {prefix ? <span className={styled.prefix}>{prefix}</span> : null}
+      <input
+        className={styled['innert-input']}
+        {...props}
+      />
+      {suffix ? <span className={styled.suffix}>{suffix}</span> : null}
+    </span>
+  }
+
+  renderInput() {
+    const { placeholder, defaultValue, value, disabled, readOnly, className, size } = this.props
+    const props = {
+      placeholder,
+      defaultValue,
+      value,
+      disabled,
+      readOnly,
+      onFocus: this.focusHandle.bind(this),
+      onBlur: this.blurHandle.bind(this),
+      onChange: this.changeHandle.bind(this),
+      onKeyPress: this.keyPressHandle.bind(this)
     }
-    return <input className={this.classes()} {...props}></input>
+    const classes = classMerge(
+      className,
+      styled.input,
+      styled[size]
+    )
+    return <input className={classes} {...props} />
+  }
+
+  render() {
+    const { prefix, suffix } = this.props
+    if (prefix || suffix) {
+      return this.renderAffixInput()
+    }
+    return this.renderInput()
   }
 }
+
+namespace Input {
+  export interface Props {
+    size?: InputSize
+    disabled?: boolean
+    defaultValue?: string
+    value?: string
+    placeholder?: string
+    prefix?: React.ReactNode
+    suffix?: React.ReactNode
+    className?: string
+    readOnly?: boolean
+    onFocus?: React.FocusEventHandler<HTMLInputElement>
+    onBlur?: React.FocusEventHandler<HTMLInputElement>
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
+    onPressEnter?: React.KeyboardEventHandler<HTMLInputElement>
+    onKeyPress?: React.KeyboardEventHandler<HTMLInputElement>
+  }
+  export const Size = InputSize
+}
+
 export default Input
---
+++ test/Input.test.tsx
@@ -0,0 +1,123 @@
+import { Input, Icon } from '../src'
+import * as Mock from 'mockjs'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import styled from '@/style/component/Input.scss'
+import styledIcon from '@/style/component/Icon.scss'
+import * as sinon from 'sinon'
+import { Simulate } from 'react-dom/test-utils'
+
+chai.should()
+
+let container: HTMLDivElement
+const inputClass = `.${styled.input}`
+
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  container.remove()
+  container = null
+})
+
+describe('Input', () => {
+  it('可以导入', () => {
+    Input.should.exist
+  })
+
+  describe('设置size', () => {
+    Input.Size.forEach(size => {
+      it(`可以设置${size}`, () => {
+        ReactDOM.render(<Input size={size} />, container)
+        container.querySelector(inputClass).classList.contains(styled[size]).should.be.ok
+      })
+    })
+  })
+
+  it(`可以设置disabled`, () => {
+    ReactDOM.render(<Input disabled />, container)
+    container.querySelector(inputClass).hasAttribute('disabled').should.be.ok
+  })
+
+  it('可以设置defaultValue', () => {
+    const value = Mock.Random.sentence(1, 2)
+    ReactDOM.render(<Input defaultValue={value} />, container)
+    const input: HTMLInputElement = container.querySelector(inputClass)
+    input.defaultValue.should.equal(value)
+  })
+
+  it('可以设置value', () => {
+    const value = Mock.Random.sentence(1, 2)
+    ReactDOM.render(<Input value={value} />, container)
+    const input: HTMLInputElement = container.querySelector(inputClass)
+    input.value.should.equal(value)
+  })
+
+  it('可以设置placeholder', () => {
+    const value = Mock.Random.sentence(1, 2)
+    ReactDOM.render(<Input placeholder={value} />, container)
+    const input: HTMLInputElement = container.querySelector(inputClass)
+    input.placeholder.should.equal(value)
+  })
+
+  it('可以设置className', () => {
+    const className = Mock.Random.word(3, 5)
+    ReactDOM.render(<Input className={className} />, container)
+    container.querySelector(inputClass).classList.contains(className).should.be.ok
+  })
+
+  it('可以设置readOnly', () => {
+    ReactDOM.render(<Input readOnly />, container)
+    container.querySelector(inputClass).hasAttribute('readonly').should.be.ok
+  })
+
+  it('可以设置prefix', () => {
+    ReactDOM.render(<Input prefix={<Icon type='user' />} />, container)
+    container.querySelector(inputClass).querySelector(`.${styled.prefix}`)
+      .querySelector(`.${styledIcon.icon}`).should.exist
+  })
+
+  it('可以设置suffix', () => {
+    ReactDOM.render(<Input suffix={<Icon type='user' />} />, container)
+    container.querySelector(inputClass).querySelector(`.${styled.suffix}`)
+      .querySelector(`.${styledIcon.icon}`).should.exist
+  })
+
+  it('可以触发focus', () => {
+    const onFocus = sinon.fake()
+    ReactDOM.render(<Input onFocus={onFocus} />, container)
+    Simulate.focus(container.querySelector(inputClass))
+    onFocus.should.has.been.called
+  })
+
+  it('可以触发blur', () => {
+    const onBlur = sinon.fake()
+    ReactDOM.render(<Input onBlur={onBlur} />, container)
+    Simulate.blur(container.querySelector(inputClass))
+    onBlur.should.has.been.called
+  })
+
+  it('可以触发change', () => {
+    const onChange = sinon.fake()
+    ReactDOM.render(<Input onChange={onChange} />, container)
+    Simulate.change(container.querySelector(inputClass))
+    onChange.should.has.been.called
+  })
+
+  it('可以触发pressEnter', () => {
+    const onPressEnter = sinon.fake()
+    ReactDOM.render(<Input onPressEnter={onPressEnter} />, container)
+    Simulate.keyPress(container.querySelector(inputClass), { key: 'Enter' })
+    onPressEnter.should.has.been.called
+  })
+
+  it('可以触发keyPress', () => {
+    const onKeyPress = sinon.fake()
+    ReactDOM.render(<Input onKeyPress={onKeyPress} />, container)
+    Simulate.keyPress(container.querySelector(inputClass), { key: Mock.Random.character() })
+    onKeyPress.should.has.been.called
+  })
+
+})
